cb11ce088b638a40bedc7427045a61ae9d663d14
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-12 18:41:25 +0500

    add message component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Message from './message.js';
 
 
 
 function App() {
+  let [count,setcount]=useState(0);
+
   return (
     <div className="App">
-      <h1>test</h1>
+      <Message counter={count}/>
+      <button onClick={()=>setcount(count+1)}>
+        Value +</button>
     </div>
   );
 }
---
+++ src/message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Message(props){
+    return(
+        <h2>This is a counter : {props.counter} </h2>
+    )
+}
+
+export default Message;
