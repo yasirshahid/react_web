e0484b0420764ff93dc81b33a5de0351223c5d7b
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-12 18:24:08 +0500

    update 12june

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello World "this my test web_page 6"</h1>
+      <h1>test</h1>
     </div>
   );
 }
